f41aeabee1f9141cef69c3befef2cfbf2fce833d
thotel <143996730+thotel@users.noreply.github.com>
2023-09-22 10:13:18 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,15 +9,6 @@ echo "[default]
 aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
 
-# Override default AWS endpoint if user sets AWS_S3_ENDPOINT.
-if [ -n "$AWS_S3_ENDPOINT" ]; then
-  ENDPOINT_APPEND="--endpoint-url $AWS_S3_ENDPOINT"
-fi
-
-pwd
-ls -la
-
-aws s3 cp ${FILE} s3://${AWS_S3_BUCKET} \
-            ${ENDPOINT_APPEND} $*
+aws s3 cp ${FILE} s3://${AWS_S3_BUCKET} --endpoint-url ${AWS_S3_ENDPOINT} $*
 
 rm -rf ~/.aws
